f65dc2d5d3b7a948c795999303ce2940457f1e97
Ateeq-Ur-Rehman01 <akbarali85930@gmail.com>
2021-07-25 08:43:44 +0530

    first commit

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,38 @@
+//primitive datatype
+//string
+//number
+//symbol
+//undefined
+//null
+//boolean
+
+
+var a="xyz";
+var b=4;
+var c;
+var d=null;
+var e=true;
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
+//hoisting:hoisting in javascript refers to the process whereby the compiler allocates memory for
+//variable and function declaration prior to execution of the code
+//example:
+
+var x;
+a=100;
+console.log(x)
+
+var y;
+console.log(y)
